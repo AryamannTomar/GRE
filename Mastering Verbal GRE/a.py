b9abe09edfdbe484063b649ccc9734e740059847
Aryaman Tomar <aryaman.tomar@gstn.org.in>
2024-04-12 12:04:11 +0530

    Initial Commit

---
+++ Mastering Verbal GRE/a.py
@@ -0,0 +1,33 @@
+import os
+l=[
+    'Intro & Vocab Buidling',
+    'Vocab Building Part II',
+    'Text Completion Part I',
+    'Text Completion Part II',
+    'Short Passages',
+    'Medium Passages',
+    'Long Passages',
+    'Critical Reasoning Part I',
+    'Critical Reasoning Part II',
+    'Critical Reasoning Part III',
+    'Sentence Equivalence',
+    'Analyse the Issue (AWA)',
+    'Analyse an Argument (AWA)',
+    'Doubt Solving & Refresher Part I',
+    'Doubt Solving & Refresher Part II',
+]
+
+for i in range(len(l)):
+    if(len(str(i))<2):
+        l[i]=f'0{i}_{l[i]}'
+    else:
+        l[i]=f'{i}_{l[i]}'
+
+# x=input()
+# while(int(x)!=-1):
+#     l.append(x)
+#     x=input("Please Name It: ")
+
+for i in l:
+    with open(f'{i}.txt', 'w') as f:
+        pass
